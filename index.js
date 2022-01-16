038542b5879102808f00bc0a478c8b10a8a3726c
Mwau stephen <stephenmwau41@gmail.com>
2022-01-15 20:24:37 -0800

    started implementing smooth scrolling to royaltech website

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 console.log("hello stephen");
 console.log("welocme to javascript i hope you like it");
+// making smooth scrolling animation
+const allLinks = document.querySelectorAll("a:link");
+// console.log(allLinks);
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    if (href == "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
